d4e8acb7a4bcc387fadecbb953aa8e7dd02b2c1a
Tero <tero.tolonen@adenova.fi>
2017-05-04 18:19:01 +0200

    Compiler

--- compiler/TestCodeCompiler.clj
+++ compiler/TestCodeCompiler.clj
@@ -4,9 +4,9 @@
     (Import "writer.clj")
     (Import "parser.clj")
 
-	(Import "RangerCommonWriter.clj")
-	(Import "RangerES5Writer.clj")
-	(Import "RangerES5Writer.clj")
+    (Import "RangerCommonWriter.clj")
+    (Import "RangerES5Writer.clj")
+    (Import "RangerES5Writer.clj")
 
     ; node tooltest.js TestCodeCompiler.clj TestCodeCompiler compile tools/compile ranger-compile 1
     ; There could be annotations to allow compiling shell scripts out from the file
@@ -104,147 +104,16 @@
                     (print "Compilation error occurred !!")
                 )
 
-                ; should evaluate the fileName
-                ; external dependencies could be marked
-
                 (def read_filename:string "./TestCodeCompiler.clj" )
 
                 (if ( > (shell_arg_cnt _) 0 )
                     (= read_filename (shell_arg 0))
                 )
 
-				(def c:string (file_read "." read_filename))
-
-				(def code:SourceCode (new SourceCode (c)))
-                (= code.filename read_filename)
-				(def parser:RangerLispParser (new RangerLispParser (code)))
-				(call parser parse ())
-
-                (if parser.had_error
-                    (
-                        (return _)
-                    )
-                )
-
-				; --> parsing the file and dependencies
-
-                (def appCtx:RangerAppWriterContext (new RangerAppWriterContext()))
-
-                ; test using the JS writer
-                (def cwr:RangerES5Writer (new RangerES5Writer ()))
-
-				(def node:CodeNode parser.rootNode)
-
-				(def wr:CodeWriter (new CodeWriter ()))
-
-				(call cwr CollectMethods (node appCtx wr))
-                (call cwr StartCodeWriting (node appCtx wr))
-
-                (def had_errors:boolean false)
-                (if (== (array_length appCtx.compilerErrors) 0)
-                    (
-                        ; (print "No compiler errors or warnings")
-                        (call wr newline ())
-
-                        (if ( > (shell_arg_cnt _) 0 )
-                            (
-
-                            )
-                            (
-                                (call wr out ("(new TestCodeCompiler()).test1()" true))
-                                
-                            )
-                        )
-                        (print (call wr getCode ()))                       
-                    )
-                    (
-                        (= had_errors true)
-                        (print "Had following compiler errors:")
-                        (for appCtx.compilerErrors e:RangerCompilerError i 
-                            (
-                                (def line_index:int (call e.node getLine ()))
-                                (print (+ (call e.node getFilename ()) " Line: " line_index))
-                                (print e.description)
-                                (print (call e.node getLineString(line_index)))
-                            )
-                        )                    
-                    )
-                )
-
-                ;(print "classes")
-                ;(for appCtx.definedClassList name:string ii
-                ;    (
-                ;        (print name)
-                ;        (def cc:RangerAppClassDesc (call appCtx findClass (name)))
-                ;        (for cc.variables vv:RangerAppParamDesc i2
-                ;            (print (+ "def " vv.name))
-                ;        )
-                ;        (for cc.methods mm:RangerAppParamDesc i2
-                ;            (print (+ "method::" mm.name))
-                ;        )
-                ;
-                ;    )
-                ;)
-
-
+                (compile read_filename)
 
             ))            
-                    
-            ( PublicMethod run:void () 
-                (
-                ; should evaluate the fileName
-                ; external dependencies could be marked
-
-                (def fileSystem:CodeFileSystem (new CodeFileSystem ()))
-                ; 
-
-                (print (shell_arg 0))
-
-				(def c:string (file_read "." "./TestFS.clj"))
-				(def code:SourceCode (new SourceCode (c)))
-				(def parser:RangerLispParser (new RangerLispParser (code)))
-				(call parser parse ())
-
-				; --> parsing the file and dependencies
-
-                (def appCtx:RangerAppWriterContext (new RangerAppWriterContext()))
-
-                ; test using the JS writer
-				(def cwr:RangerES5Writer (new RangerES5Writer ()))
-
-				(def node:CodeNode parser.rootNode)
-
-                (def cf:CodeFile (call fileSystem getFile ("ranger/test1" "fstest.js")) )
-                (def wr:CodeWriter (call cf getWriter ()))
-
-				; (def wr:CodeWriter (new CodeWriter ()))
-
-				(call cwr CollectMethods (node appCtx wr))
-                (call cwr StartCodeWriting (node appCtx wr))
-
-                ; (call ww out ( (call wr getCode ()) false ))
-
-                (call fileSystem saveTo ("output"))
-
-                ;(print "classes")
-                ;(for appCtx.definedClassList name:string ii
-                ;    (
-                ;        (print name)
-                ;        (def cc:RangerAppClassDesc (call appCtx findClass (name)))
-                ;        (for cc.variables vv:RangerAppParamDesc i2
-                ;            (print (+ "def " vv.name))
-                ;        )
-                ;        (for cc.methods mm:RangerAppParamDesc i2
-                ;            (print (+ "method::" mm.name))
-                ;        )
-                ;
-                ;    )
-                ;)
-
-                ; (print (call wr getCode ()))
-                
-
-            ))            
+                              
         )
     )
     
